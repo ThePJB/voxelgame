bf1c570e140b5bdbd4e6b0dcc969da3db43beb95
kennoath <zclpatrick@gmail.com>
2020-06-18 10:51:08 +1000

    fix cookedness

--- shader.h
+++
@@ -1,6 +0,0 @@
-#ifndef SHADER_H
-#define SHADER_H
-
-unsigned int make_shader(char *path, unsigned int type);
-
-#endif
--- texture.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEXTURE_H
-#define TEXTURE_H
-
-unsigned int load_texture(char *path);
-
-#endif
